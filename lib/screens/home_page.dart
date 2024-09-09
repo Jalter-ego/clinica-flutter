1d5157bdfacea496a3d93e1aa39b2f46e64a3d9f
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2024-09-09 17:27:28 -0400

    ..

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,15 +10,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+   int _selectedIndex = 0;
+
+  // Lista de widgets para cada página de la barra de navegación
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Inicio')),
+    Center(child: Text('Horarios')),
+    Center(child: Text('Información')),
+    Center(child: Text('Perfil')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Página Principal'),
       ),
       drawer: const AppDrawer(),
-      body: const Center(
-        child: Text('Contenido principal'),
+      body: _pages[_selectedIndex], // Cambia el contenido según el índice seleccionado
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Horarios',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'Información',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightBlue, // Cambia el color del icono seleccionado
+        unselectedItemColor: Colors.lightBlue, // Color de los íconos no seleccionados
+        onTap: _onItemTapped,
       ),
     );
   }
